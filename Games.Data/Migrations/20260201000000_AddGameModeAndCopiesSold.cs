d78c9aaaf1161c1162c7c6ccf662f6156b349054
VictoriaBuchko <123644516+VictoriaBuchko@users.noreply.github.com>
2026-05-12 14:23:32 +0300

    Add GameMode and CopiesSold columns to Games table

--- Games.Data/Migrations/20260201000000_AddGameModeAndCopiesSold.cs
+++ Games.Data/Migrations/20260201000000_AddGameModeAndCopiesSold.cs
@@ -10,15 +10,13 @@ namespace Games.Data.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            // Завдання 4: додаємо режим гри
             migrationBuilder.AddColumn<string>(
                 name: "GameMode",
                 table: "Games",
                 type: "TEXT",
                 nullable: false,
                 defaultValue: "");
-
-            // Завдання 4: додаємо кількість проданих копій
+                
             migrationBuilder.AddColumn<int>(
                 name: "CopiesSold",
                 table: "Games",
